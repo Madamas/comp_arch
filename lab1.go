a17b979a14ccecc221ab9e3c793237973f8390f9
Vadim Maslov <mrvadamas@gmail.com>
2018-11-12 01:15:08 +0200

    updated code; added tests

--- lab1.go
+++ lab1.go
@@ -2,92 +2,12 @@ package main
 
 import (
 	"fmt"
+	"set"
 )
 
-type storage map[interface{}]bool
-
-func NewSet() *storage {
-	st := make(storage)
-	return &st
-}
-
-func (set *storage)Length() int {
-	return len(*set)
-}
-
-func (set *storage)Add(item interface{}) bool {
-	_, entry := (*set)[item]
-
-	if(entry) {
-		return false
-	}
-
-	(*set)[item] = true
-
-	return true
-}
-
-func (set *storage)Has(item interface{}) bool {
-	_, entry := (*set)[item]
-
-	if(entry) {
-		return true
-	} else {
-		return false
-	}
-}
-
-func (set *storage)Intersect(input *storage) *storage {
-	intersection := NewSet()
-
-	if(set.Length() <= input.Length()) {
-		for elem := range *set {
-			if(input.Has(elem)) {
-				intersection.Add(elem)
-			}
-		}
-	} else {
-		for elem := range *input {
-			if(set.Has(elem)) {
-				intersection.Add(elem)
-			}
-		}
-	}
-
-	return intersection
-}
-
-func (set *storage)Difference(input *storage) *storage {
-	difference := NewSet()
-
-	for elem := range *set {
-		if(!input.Has(elem)) {
-			difference.Add(elem)
-		}
-	}
-
-	return difference
-}
-
-func (set *storage)Union(input *storage) *storage {
-	union := NewSet()
-
-	for elem := range *set {
-		union.Add(elem)
-	}
-
-	for elem := range *input {
-		if(!union.Has(elem)) {
-			union.Add(elem)
-		}
-	}
-
-	return union
-}
-
 func main() {
-	set1 := NewSet()
-	set2 := NewSet()
+	set1 := set.NewSet()
+	set2 := set.NewSet()
 
 	for i := 0; i < 5; i++ {
 		set1.Add(i)
